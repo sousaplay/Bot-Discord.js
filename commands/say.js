cbc84e46ed561aef2527d65df77f8cb2d405527f
Rodrigo Sousa <85514941+sousaplay@users.noreply.github.com>
2022-01-02 13:06:42 +0000

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+exports.run = async (client, message, args) => {      
+  const sayMessage = args.join(' ');
+  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(`**🚨 | Você não tem permissão para utilizar esse comando!**`)
+  message.delete().catch(O_o => {});
+      const embed = new Discord.MessageEmbed()
+      .setColor('')
+      .setDescription(sayMessage)
+  message.channel.send(embed);
+}
